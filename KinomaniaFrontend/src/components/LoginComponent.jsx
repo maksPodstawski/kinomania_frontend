8041819262ffdbcb8a912c94d95cf276bc4a12a4
Mateusz Łatka <kezsteam@gmail.com>
2024-05-14 15:45:58 +0200

    Update the LoginComponent.jsx and loggingPageStyles.css

--- KinomaniaFrontend/src/components/LoginComponent.jsx
+++ KinomaniaFrontend/src/components/LoginComponent.jsx
@@ -1,28 +1,34 @@
-import React, {useState} from 'react';
-import SendLoginRequest from "../service/SendLoginRequest.jsx";
-import '../styles/loggingPageStyles.css'
-import {jwtDecode} from "jwt-decode";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from 'react';
+import SendLoginRequest from '../service/SendLoginRequest.jsx';
+import '../styles/loggingPageStyles.css';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 function LoginComponent() {
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const [token, setToken] = useState(null);
+    const [showPassword, setShowPassword] = useState(false); // New state to toggle password visibility
 
     const navigate = useNavigate();
 
     const handleLoginChange = (event) => {
         setLogin(event.target.value);
-    }
+    };
 
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
-    }
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     const saveTokenToLocalStorage = (token) => {
         localStorage.setItem('token', token);
-        const decoded = jwtDecode(token)
-        localStorage.setItem('username', decoded["username"]);
-        localStorage.setItem('authorities', decoded["authorities"]);
+        const decoded = jwtDecode(token);
+        localStorage.setItem('username', decoded['username']);
+        localStorage.setItem('authorities', decoded['authorities']);
         console.log(localStorage.getItem('authorities'));
     };
 
@@ -30,17 +36,39 @@ function LoginComponent() {
         const token = await SendLoginRequest(login, password);
         setToken(token);
         saveTokenToLocalStorage(token);
-        if(token !== ""){
+        if (token !== '') {
             navigate('/');
         }
-    }
+    };
 
     return (
         <div className="login-form">
             <h2>Login:</h2>
-            <input id="login" name="login" type="text" placeholder="Wprowadź login" required onChange={handleLoginChange}/>
-            <input id="password" name="password" type="password" placeholder="Wprowadź hasło" required onChange={handlePasswordChange}/>
-            <button onClick={handleSubmit} type="submit">Zaloguj się</button>
+            <input
+                id="login"
+                name="login"
+                type="text"
+                placeholder="Wprowadź login"
+                required
+                onChange={handleLoginChange}
+            />
+            <div className="input-container">
+                <input
+                    id="password"
+                    name="password"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Wprowadź hasło"
+                    required
+                    onChange={handlePasswordChange}
+                />
+                <span className="toggle-password" onClick={togglePasswordVisibility}>
+        {showPassword ? <i className="gg-eye-alt"></i> : <i className="gg-eye"></i>}
+    </span>
+            </div>
+            <br />
+            <button onClick={handleSubmit} type="submit">
+                Zaloguj się
+            </button>
             {token && <p>Token: {token}</p>}
         </div>
     );
